d89f9620db14352fa19c3ed654157bd10ac2d8a5
cchekal <chrischekal@gmail.com>
2017-04-07 09:59:15 -0500

    Add files via upload

---
+++ addplane.js
@@ -0,0 +1,59 @@
+function addSingle(but){
+    var row = but.parentElement.parentElement;
+    addRow(row);
+    checkErrors();
+}
+
+/*
+function updateAll(but){
+    var table = but.parentElement.parentElement.firstElementChild.firstElementChild
+    var row;
+    for(var i = 1;i < table.firstElementChild.childElementCount - 1; i++){
+	    row = document.getElementById("r"+ String(i));
+	    updateRow(row);
+    }
+    checkErrors();
+}*/
+
+function addRow(row){
+    console.log(row);
+    var model = row.querySelector(".modelCrit").value;
+    var ecnum = row.querySelector(".ecnumCrit").value;
+    var fjcnum = row.querySelector(".fjcnumCrit").value;
+    var reoccurrence = row.querySelector(".reoccurrenceCrit").value;
+
+    console.log("model: "+model);
+    console.log("ecnum: "+ecnum);
+    console.log("fjcnum: "+fjcnum);
+    console.log("reoccurrence: "+reoccurrence);
+    
+    document.getElementById("invalidInput").style.display = "none";
+    
+    if(reoccurrence == "" || model == "" || ecnum == "" || fjcnum == "") {
+        flag = true;
+        row.bgColor = "#FF0000";
+    }
+    else{     
+        row.bgColor = "transparent";
+        var address = "/admin/addPlane?model="+model+"&ecnum="+ecnum+"&fjcnum="+fjcnum+"&reoccurrence="+reoccurrence;
+        console.log("Address: "+address);
+    }
+}
+
+function checkErrors(){
+    var allErrors = document.getElementsByClassName("updateButton")
+    var flag = false;
+    for(var i = 0; i < allErrors.length;i++){
+        if(allErrors[i].parentElement.parentElement.bgColor == "#FF0000"){
+	        flag = true;
+        }
+    }
+    var error = document.getElementById("invalidInput");
+    if(flag){
+        error.style.display = "block";
+    }
+    else{
+        error.style.display = "none";
+    }
+    $('.success').fadeIn().delay(2000).fadeOut(1000);
+}
