2a1847fe184dc716c1859bacd2e440e0bcbc8f76
willem-dejong <willem-dejong@uiowa.edu>
2017-04-13 01:18:52 -0500

    Add files via upload

--- IowaAir/IAADB.sql
+++ IowaAir/IAADB.sql
@@ -90,7 +90,7 @@ CREATE TABLE `flights` (
   `fc_seats_available` int(11) NOT NULL,
   `fc_seats_booked` int(11) NOT NULL,
   `fc_cost_per_seat` float NOT NULL,
-  `plane_id` varchar(25) NOT NULL,
+  `plane_id` int(11) NOT NULL,
   `origin_port` varchar(25) NOT NULL,
   `destined_port` varchar(25) NOT NULL,
   `Gate` varchar(25) DEFAULT NULL,
@@ -99,8 +99,9 @@ CREATE TABLE `flights` (
   KEY `orig_idx` (`origin_port`),
   KEY `dest_idx` (`destined_port`),
   CONSTRAINT `dest` FOREIGN KEY (`destined_port`) REFERENCES `airports` (`portid`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `orig` FOREIGN KEY (`origin_port`) REFERENCES `airports` (`portid`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+  CONSTRAINT `orig` FOREIGN KEY (`origin_port`) REFERENCES `airports` (`portid`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `plane` FOREIGN KEY (`plane_id`) REFERENCES `planes` (`plane_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -109,7 +110,7 @@ CREATE TABLE `flights` (
 
 LOCK TABLES `flights` WRITE;
 /*!40000 ALTER TABLE `flights` DISABLE KEYS */;
-INSERT INTO `flights` VALUES (1,'3rddw','2017-07-12 07:45:00','2017-07-12 08:45:00',100,40,500,25,24,1000,'1','ORD','CID',NULL),(2,'df4','2017-07-12 11:45:00','2017-07-12 13:45:00',45,60,412,0,0,0,'2','CID','ORD',NULL),(3,'dfee','2017-07-12 14:45:00','2017-07-12 16:45:00',33,22,245,0,0,0,'2','ORD','OSS',NULL),(4,'s4r','2017-07-12 03:45:00','2017-07-12 05:45:00',90,15,412,0,0,0,'1','CID','ORD',NULL),(5,'sffaf','2017-07-12 23:45:00','2017-07-13 00:40:00',4,90,111,0,0,0,'1','ORD','CID',NULL),(6,'e43','2017-07-12 20:45:00','2017-07-12 21:45:00',12,6,111,11,5,1222,'3','CID','OSS',NULL);
+INSERT INTO `flights` VALUES (1,'3rdd','2017-07-12 07:45:00','2017-07-12 08:45:00',100,40,500,25,24,1000,1,'ORD','CID',NULL),(2,'df4','2017-07-12 11:45:00','2017-07-12 13:45:00',75,60,412,10,0,1000,2,'CID','ORD',NULL),(3,'dfee','2017-07-12 14:45:00','2017-07-12 16:45:00',33,22,245,10,0,1000,2,'ORD','OSS',NULL),(4,'s4r','2017-07-12 03:45:00','2017-07-12 05:46:00',90,15,412,10,0,1011,1,'CID','ORD',NULL),(5,'sffaf','2017-07-12 23:45:00','2017-07-13 00:40:00',100,90,111,10,0,1233,1,'ORD','CID',NULL),(6,'e43','2017-07-12 20:45:00','2017-07-12 21:45:00',12,6,111,11,5,1222,3,'CID','OSS',NULL),(13,'3r43r','2017-06-06 04:00:00','2017-06-06 06:00:00',10,10,10,10,10,10,1,'CID','ORD',NULL),(14,'3r43r','2017-07-06 07:00:00','2017-07-06 08:00:00',10,10,10,10,10,10,1,'CID','ORD',NULL),(15,'3r43r','2017-07-07 07:00:00','2017-07-07 08:00:00',10,10,10,10,10,10,1,'CID','ORD',NULL),(16,'3r43r','2017-07-08 07:00:00','2017-07-08 08:00:00',10,10,10,10,10,10,1,'CID','ORD',NULL),(17,'3r43r','2017-07-09 07:00:00','2017-07-09 08:00:00',10,10,10,10,10,10,1,'CID','ORD',NULL),(18,'3r43r','2017-07-18 07:00:00','2017-07-18 08:00:00',10,10,10,10,10,10,1,'CID','ORD',NULL),(19,'3r43r','2017-07-19 07:00:00','2017-07-19 08:00:00',10,10,10,10,10,10,1,'CID','ORD',NULL),(23,'dwq','2017-04-27 01:00:00','2017-04-27 02:00:00',11,11,11,11,11,11,1,'CID','ORD',NULL),(26,'adwa','2017-04-26 01:00:00','2017-04-26 02:00:00',11,11,11,11,11,11,1,'CID','ORD',NULL);
 /*!40000 ALTER TABLE `flights` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -126,7 +127,7 @@ CREATE TABLE `planes` (
   `fc_num_seats` int(11) NOT NULL,
   `ec_num_seats` int(11) NOT NULL,
   PRIMARY KEY (`plane_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -135,7 +136,7 @@ CREATE TABLE `planes` (
 
 LOCK TABLES `planes` WRITE;
 /*!40000 ALTER TABLE `planes` DISABLE KEYS */;
-INSERT INTO `planes` VALUES (1,'boeing 474',122,122),(2,'boeing 747',100,211),(3,'boeing 456',100,122),(4,'boeing 456',100,100),(5,'xdgsd',100,100);
+INSERT INTO `planes` VALUES (1,'boeing 474',122,122),(2,'boeing 747',100,211),(3,'boeing 456',100,122),(4,'boeing 456',100,100),(5,'xdgs',100,100),(6,'boeing 747',25,100),(7,'boeing 747',25,100),(8,'boeing 747',25,100);
 /*!40000 ALTER TABLE `planes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -201,6 +202,61 @@ DELIMITER ;
 --
 -- Dumping routines for database 'iowaair'
 --
+/*!50003 DROP PROCEDURE IF EXISTS `addFlight` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`admin`@`localhost` PROCEDURE `addFlight`(fnum varchar(50),dep datetime,arv datetime,ecsa int,ecsb int,eccps float,fcsa int,fcsb int,fccps float,am varchar(50),org varchar(25),dest varchar(25),g varchar(25),inter int,interType varchar(25),numTime int)
+BEGIN
+DECLARE oop CONDITION FOR SQLSTATE '45000';
+DECLARE EXIT HANDLER FOR SQLEXCEPTION 
+    BEGIN
+        ROLLBACK;
+		SIGNAL oop;
+    END;
+START TRANSACTION;
+
+
+set @pid=(select plane_id from iowaair.planes as f where f.plane_id not in (SELECT distinct plane_id FROM iowaair.flights where departure_time<=arv+interval 5 hour and arrival_time>=dep-interval 5 hour) and model=am limit 1);
+insert into iowaair.flights(Flight_num,departure_time,arrival_time,ec_seats_available,ec_seats_booked,ec_cost_per_seat,fc_seats_available,fc_seats_booked,fc_cost_per_seat,plane_id,origin_port,destined_port,gate)
+values (fnum,dep,arv,ecsa,ecsb,eccps,fcsa,fcsb,fccps,@pid,org,dest,g);
+set @i=0;
+WHILE @i<numTime DO
+if interType="minutes" then
+set @pid=(select plane_id from iowaair.planes as f where f.plane_id not in (SELECT distinct plane_id FROM iowaair.flights where departure_time<=arv+interval inter minute+interval 5 hour and arrival_time>=dep+interval inter minute-interval 5 hour) and model=am limit 1);
+insert into iowaair.flights(Flight_num,departure_time,arrival_time,ec_seats_available,ec_seats_booked,ec_cost_per_seat,fc_seats_available,fc_seats_booked,fc_cost_per_seat,plane_id,origin_port,destined_port,gate)
+values (fnum,dep+interval inter minute,arv+interval inter minute,ecsa,ecsb,eccps,fcsa,fcsb,fccps,@pid,org,dest,g);
+elseif interType="hours" then
+set @pid=(select plane_id from iowaair.planes as f where f.plane_id not in (SELECT distinct plane_id FROM iowaair.flights where departure_time<=arv+interval inter hour+interval 5 hour and arrival_time>=dep+interval inter hour-interval 5 hour) and model=am limit 1);
+insert into iowaair.flights(Flight_num,departure_time,arrival_time,ec_seats_available,ec_seats_booked,ec_cost_per_seat,fc_seats_available,fc_seats_booked,fc_cost_per_seat,plane_id,origin_port,destined_port,gate)
+values (fnum,dep+interval inter hour,arv+interval inter hour,ecsa,ecsb,eccps,fcsa,fcsb,fccps,@pid,org,dest,g);
+elseif interType="days" then
+set @pid=(select plane_id from iowaair.planes as f where f.plane_id not in (SELECT distinct plane_id FROM iowaair.flights where departure_time<=arv+interval inter day+interval 5 hour and arrival_time>=dep+interval inter day-interval 5 hour) and model=am limit 1);
+insert into iowaair.flights(Flight_num,departure_time,arrival_time,ec_seats_available,ec_seats_booked,ec_cost_per_seat,fc_seats_available,fc_seats_booked,fc_cost_per_seat,plane_id,origin_port,destined_port,gate)
+values (fnum,dep+interval inter day,arv+interval inter day,ecsa,ecsb,eccps,fcsa,fcsb,fccps,@pid,org,dest,g);
+elseif interType="months" then
+set @pid=(select plane_id from iowaair.planes as f where f.plane_id not in (SELECT distinct plane_id FROM iowaair.flights where departure_time<=arv+interval inter month+interval 5 hour and arrival_time>=dep+interval inter month-interval 5 hour) and model=am limit 1);
+insert into iowaair.flights(Flight_num,departure_time,arrival_time,ec_seats_available,ec_seats_booked,ec_cost_per_seat,fc_seats_available,fc_seats_booked,fc_cost_per_seat,plane_id,origin_port,destined_port,gate)
+values (fnum,dep+interval inter month,arv+interval inter month,ecsa,ecsb,eccps,fcsa,fcsb,fccps,@pid,org,dest,g);
+else
+ROLLBACK;
+SIGNAL oop;
+end if;
+set @i=@i+1;
+END WHILE;
+COMMIT;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `getFlights1` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -227,7 +283,7 @@ f.ec_cost_per_seat*pass as ec_price1,
 p.model as model1,
 f.flightID as flightID1,
 0 as num_stops,
-f.arrival_time-f.departure_time as tot_time,
+timediff(f.arrival_time,f.departure_time) as tot_time,
 f.fc_cost_per_seat*pass as tot_fc_price,
 f.ec_cost_per_seat*pass as tot_ec_price,
 f.fc_seats_available>(f.fc_seats_booked+pass) as has_fc,
@@ -243,7 +299,7 @@ f.origin_port=ap1.portid and
 f.destined_port=ap2.portid and
 f.origin_port=org and
 f.destined_port=dest and
-datediff(f.departure_time,ddate)=0 and
+f.departure_time<=ddate+ interval 1.5 day and
 f.departure_time>=ddate and
 f.departure_time>=now()+interval 1 hour and
 ((f.fc_seats_available>(f.fc_seats_booked+pass)) or (f.ec_seats_available>(f.ec_seats_booked+pass)));
@@ -291,7 +347,7 @@ f2.ec_cost_per_seat*pass as ec_price2,
 p2.model as model2,
 f2.flightID as flightID2,
 1 as num_stops,
-f2.arrival_time-f.departure_time as tot_time,
+timediff(f2.arrival_time,f.departure_time) as tot_time,
 (f.fc_cost_per_seat+f2.fc_cost_per_seat)*pass as tot_fc_price,
 (f.ec_cost_per_seat+f2.ec_cost_per_seat)*pass as tot_ec_price,
 ((f.fc_seats_available>(f.fc_seats_booked+pass)) and (f2.fc_seats_available>(f2.fc_seats_booked+pass))) as has_fc,
@@ -313,9 +369,10 @@ f2.plane_id=p2.plane_id and
 f2.origin_port=ap12.portid and
 f2.destined_port=ap22.portid and
 f.origin_port=org and
+f.destined_port!=dest and
 f.destined_port=f2.origin_port and
 f2.destined_port=dest and
-datediff(f.departure_time,ddate)=0 and
+f.departure_time<=ddate+ interval 1.5 day and
 f.departure_time>=ddate and
 f.departure_time>=now()+interval 1 hour and
 f2.departure_time>=DATE_ADD(f.arrival_time, INTERVAL 30 MINUTE) and
@@ -377,7 +434,7 @@ f3.ec_cost_per_seat*pass as ec_price3,
 p3.model as model3,
 f3.flightID as flightID3,
 2 as num_stops,
-f3.arrival_time-f.departure_time as tot_time,
+timediff(f3.arrival_time,f.departure_time) as tot_time,
 (f.fc_cost_per_seat+f2.fc_cost_per_seat+f3.fc_cost_per_seat)*pass as tot_fc_price,
 (f.ec_cost_per_seat+f2.ec_cost_per_seat+f3.ec_cost_per_seat)*pass as tot_ec_price,
 ((f.fc_seats_available>(f.fc_seats_booked+pass)) and (f2.fc_seats_available>(f2.fc_seats_booked+pass)) and (f3.fc_seats_available>(f3.fc_seats_booked+pass))) as has_fc,
@@ -406,16 +463,73 @@ f3.plane_id=p3.plane_id and
 f3.origin_port=ap13.portid and
 f3.destined_port=ap23.portid and
 f.origin_port=org and
+f.destined_port!=dest and
 f.destined_port=f2.origin_port and
+f2.destined_port!=dest and
+f2.destined_port!=f.origin_port and
 f2.destined_port=f3.origin_port and
 f3.destined_port=dest and
-datediff(f.departure_time,ddate)=0 and
+f.departure_time<=ddate+ interval 1.5 day and
 f.departure_time>=ddate and
 f.departure_time>=now()+interval 1 hour and
 f2.departure_time>=DATE_ADD(f.arrival_time, INTERVAL 30 MINUTE) and
 f3.departure_time>=DATE_ADD(f2.arrival_time, INTERVAL 30 MINUTE) and
 f3.departure_time<=DATE_ADD(f.arrival_time, INTERVAL 1 DAY) and
 ((f.fc_seats_available>(f.fc_seats_booked+pass) and f2.fc_seats_available>(f2.fc_seats_booked+pass) and f3.fc_seats_available>(f3.fc_seats_booked+pass)) or (f.ec_seats_available>(f.ec_seats_booked+pass) and f2.ec_seats_available>(f2.ec_seats_booked+pass) and f3.ec_seats_available>(f3.ec_seats_booked+pass)));
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `updateFlight` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`admin`@`localhost` PROCEDURE `updateFlight`(id int,fnum varchar(50),dep datetime,arv datetime,ecsa int,ecsb int,eccps float,fcsa int,fcsb int,fccps float,am varchar(50),org varchar(25),dest varchar(25),g varchar(25))
+BEGIN
+set @cmod=(select p.model from iowaair.planes as p, iowaair.flights as f where f.flightID=id and f.plane_id=p.plane_id);
+if @cmod!=am then
+set @pid=(select plane_id from iowaair.planes as f where f.plane_id not in (SELECT distinct plane_id FROM iowaair.flights where departure_time<=arv+interval 5 hour and arrival_time>=dep-interval 5 hour) and model=am limit 1);
+update iowaair.flights 
+set 
+Flight_num=fnum,
+departure_time=dep,
+arrival_time=arv, 
+ec_seats_available=ecsa,
+ec_seats_booked=ecsb,
+ec_cost_per_seat=eccps, 
+fc_seats_available=fcsa,
+fc_seats_booked=fcsb,
+fc_cost_per_seat=fccps,
+plane_id=@pid,
+origin_port=org,
+destined_port=dest,
+gate=g 
+where flightID=id;
+else
+update iowaair.flights 
+set 
+Flight_num=fnum,
+departure_time=dep,
+arrival_time=arv, 
+ec_seats_available=ecsa,
+ec_seats_booked=ecsb,
+ec_cost_per_seat=eccps, 
+fc_seats_available=fcsa,
+fc_seats_booked=fcsb,
+fc_cost_per_seat=fccps,
+origin_port=org,
+destined_port=dest,
+gate=g 
+where flightID=id;
+end if;
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -432,4 +546,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-04-10 21:25:26
+-- Dump completed on 2017-04-13  1:11:07
