2e00724fa09e05cf38a613fc82bb51ab1a700947
willem-dejong <willem-dejong@uiowa.edu>
2017-04-25 00:10:19 -0500

    Add files via upload

--- IowaAir/public/js/index.js
+++ IowaAir/public/js/index.js
@@ -4,6 +4,7 @@ function verifyAndSubmit(){            //validate for required fields
    var destination = document.getElementById("dest").value;
    var departDate = document.getElementById("date").value;
    var returnDate = document.getElementById("date2").value;
+   var pass = parseInt(document.getElementById("pass").value);
    console.log(!departDate)
    console.log(!returnDate)
    var requiredFlag = false;
@@ -88,7 +89,85 @@ function verifyAndSubmit(){            //validate for required fields
    	if(returnDate){
    		cmd=cmd+"&date2="+returnDate
    	}
-   	cmd=cmd+"&pass="+ document.getElementById("pass").value;
+   	cmd=cmd+"&pass="+ pass;
+   	window.location=cmd
+   }
+}
+function verifyAndSubmit2(){            //validate for required fields
+   //var org = document.getElementById("org").value;
+   //var destination = document.getElementById("dest").value;
+   var departDate = document.getElementById("dateb").value;
+   var returnDate = document.getElementById("date2b").value;
+   var pass = parseInt(document.getElementById("passb").value);
+   console.log(!departDate)
+   console.log(!returnDate)
+   var requiredFlag = false;
+   var invalidDate = false;
+   var portFlag=false;
+   //var portFlag2=false;
+   var depdl=departDate.split("-")
+   var depDate = new Date(parseInt(depdl[0]),parseInt(depdl[1])-1,parseInt(depdl[2]));
+   var retdl=returnDate.split("-")
+   var retDate = new Date(parseInt(retdl[0]),parseInt(retdl[1])-1,parseInt(retdl[2]));
+   var nowt=new Date;
+   var now=new Date(nowt.getFullYear(),nowt.getMonth(),nowt.getDate())
+   console.log(now);
+   console.log("Departure Date: " + depDate);
+   console.log("Arrival Date: " + retDate);
+   //document.getElementById('orglab').style.color = "black";
+   //document.getElementById('destlab').style.color = "black";
+   document.getElementById('datelabb').style.color = "black";
+   document.getElementById('date2labb').style.color = "black";
+   document.getElementById('missReqb').style.display = "none";
+   document.getElementById('dateErrb').style.display = "none";
+   document.getElementById('portErrb').style.display ="none";
+   //document.getElementById('portErr2').style.display ="none";
+   if(flights.length<2){
+       document.getElementById('portErrb').style.color = "red";
+       portFlag = true;
+   }
+   if(!departDate){
+       document.getElementById('datelabb').style.color = "red";
+       requiredFlag = true;
+   }
+   else if(now>depDate){
+       document.getElementById('datelabb').style.color = "red";
+       invalidDate = true;
+   }
+   if(!returnDate){
+       console.log("No arrival date");
+   }
+   else if(now>retDate){
+       document.getElementById('date2labb').style.color = "red";
+       invalidDate = true;	
+   }
+   else if(depDate > retDate){
+       	invalidDate = true;
+         document.getElementById('date2labb').style.color = "red";
+   }
+    
+   if (requiredFlag||invalidDate||portFlag){
+       if(requiredFlag){
+       	document.getElementById('missReqb').style.display ="block";
+       }
+       if(invalidDate){
+           document.getElementById('dateErrb').style.display = "block";
+       }
+       if(portFlag){
+           document.getElementById('portErrb').style.display = "block";
+       }
+   }
+   else{
+   	console.log("submit");
+   	var portstr=""
+   	for(i in flights){
+   		portstr=portstr+flights[i]+";"
+   	}
+   	cmd="/mapResult?ports="+portstr+"&date1="+departDate
+   	if(returnDate){
+   		cmd=cmd+"&date2="+returnDate
+   	}
+   	cmd=cmd+"&pass="+ pass;
    	window.location=cmd
    }
 }
